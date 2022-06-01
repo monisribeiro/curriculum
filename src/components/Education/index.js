94dd151c89f9713112b933d758e4b7f8250847fe
Monique Ribeiro <monisribeiro@gmail.com>
2022-06-01 08:46:49 -0400

    education and about me

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -1,9 +1,44 @@
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import './styles.css';
+import ufscar from '../../assets/ufscar.png'
+import fiap from '../../assets/fiap.png'
 
 function Education() {
   return (
-    <div className="Education">
-        <h1>This will be Education</h1>
+    <div id="education" className="Education">
+      <h1 className="text-center my-5">Education</h1>
+      <Container>
+        <Row className="justify-content-between">
+          <Col xs={12} sm={6} className="Education-card">
+            <Card style={{ width: '100%' }} bg="light" className="text-center">
+              <Card.Img variant="top" src={ufscar} height={60}  width={60}/>
+              <Card.Body>
+                <Card.Title>BS in Computer Science</Card.Title>
+                <Card.Text>
+                  <small>Mar 2012 - Dec 2015</small>
+                </Card.Text>
+                <Card.Text>
+                  UFSCAR - Universidade Federal de São Carlos
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} className="Education-card">
+            <Card style={{ width: '100%' }} bg="light" className="text-center">
+              <Card.Img variant="top" src={fiap}  height={60}  width={60}/>
+              <Card.Body>
+                <Card.Title>MBA in Software Engineering</Card.Title>
+                <Card.Text>
+                  <small>Apr 2020 - Apr 2021</small>
+                </Card.Text>
+                <Card.Text>
+                  FIAP - Faculdade de Informática e Administração Paulista
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/MoreInfo/index.js
+++ src/components/MoreInfo/index.js
@@ -1,11 +1,39 @@
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import './styles.css';
+import profile from '../../assets/4.png';
 
-function MoreInfo() {
-  return (
-    <div className="MoreInfo">
-        <h1>This will be MoreInfo</h1>
-    </div>
-  );
-}
+  function MoreInfo() {
+    return (
+      <div id="more" className="MoreInfo mt-5">
+        <Container className="text-start">
+          <Row gap={5}>
+            <Col className="more-info-img d-flex align-items-sm-center mx-3">
+              <Image src={profile} height={120} />
+            </Col>
+            <Col className="more-info-texts">
+              <h3 className="mx-3">About me</h3>
+              <ul>
+                <li>
+                  Brazilian, 28 years old, married, pet owner 
+                </li>
+                <li>
+                  Languages: Portuguese - native, English - fluent and Spanish - intermediate
+                </li>
+                <li>
+                  Location: 02210, Boston - Massachussets 
+                </li>
+                <li>
+                  Email: monisribeiro@gmail.com 
+                </li>
+                <li>
+                  Phone: +55 (15) 99703-1333 
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 
 export default MoreInfo;
